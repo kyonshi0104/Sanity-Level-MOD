ad3ee614fcab15abf917bf450b7846ff47c02e97
Kyonshi <155651185+kyonshi0104@users.noreply.github.com>
2026-03-27 20:01:00 +0900

    fix: 大量のログを削除

--- src/client/java/com/kyonshi/SanityLevelClient.java
+++ src/client/java/com/kyonshi/SanityLevelClient.java
@@ -5,7 +5,6 @@ import com.kyonshi.client.SanityShaderHandler;
 import net.fabricmc.api.ClientModInitializer;
 import com.kyonshi.network.SanitySyncPayload;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import org.slf4j.LoggerFactory;
 
 public class SanityLevelClient implements ClientModInitializer {
     public static int currentSanity = 20;
@@ -14,10 +13,8 @@ public class SanityLevelClient implements ClientModInitializer {
     public void onInitializeClient() {
         // パケット受信時の処理
         ClientPlayNetworking.registerGlobalReceiver(SanitySyncPayload.ID, (payload, context) -> {
-            LoggerFactory.getLogger("sanity-level").info(String.valueOf("currentSanity"));
             context.client().execute(() -> {
                 currentSanity = payload.sanity();
-                LoggerFactory.getLogger("sanity-level").info(String.valueOf(currentSanity));
             });
         });
 
